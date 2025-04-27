003a3be410c6011fba03f1985b9c59801d0601b2
Arnav Dutta <arnav.dutta26@gmail.com>
2025-04-27 20:22:07 +0530

    Update frameBuffer.hpp
    Enhance FrameBuffer to manage frames with indices
    
    Introduces a new `Frame` structure to encapsulate `cv::Mat` and frame index.
    Changes the internal buffer of `FrameBuffer` to store `Frame` objects,
    adds a `frameIdx_` member for tracking the current frame index,
    and updates the constructor to set a default max size of 10.
    Modifies the `pop` method to return a `Frame` and adds
    a new method `getFrameByIndex` for frame retrieval by index.

--- include/pipeline/frameBuffer.hpp
+++ include/pipeline/frameBuffer.hpp
@@ -5,19 +5,31 @@
 #include <iostream>
 #include <mutex>
 #include <deque>
+#include <condition_variable>
 #include <opencv2/opencv.hpp>
+
+
+struct Frame
+{
+    cv::Mat frame;
+    uint32_t frameIndex;
+};
 
 class FrameBuffer
 {
 private:
-    std::deque<cv::Mat> buffer_;
+    std::deque<Frame> buffer_;
     std::mutex mutex_;
-    size_t maxSize_;	
+    size_t maxSize_;
+    std::condition_variable cond_;
+	uint32_t frameIdx_;
 
 public:
-	FrameBuffer(size_t MAX_SIZE = 1):maxSize_(MAX_SIZE){}
+	FrameBuffer(size_t MAX_SIZE = 10):maxSize_(MAX_SIZE), frameIdx_(0) {}
 
 	void push(const cv::Mat& frame);
 
-	bool pop(cv::Mat& frame);
+	bool pop(Frame& outFrame);
+
+    Frame getFrameByIndex(uint32_t idx);
 };
