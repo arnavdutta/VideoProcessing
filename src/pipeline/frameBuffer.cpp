c916cd30529bab6aba90ac9f26e88afe6eca02db
Arnav Dutta <arnav.dutta26@gmail.com>
2025-04-27 20:25:09 +0530

    added getFrameByIndex function and Frame struct

--- src/pipeline/frameBuffer.cpp
+++ src/pipeline/frameBuffer.cpp
@@ -7,21 +7,41 @@
 
 void FrameBuffer::push(const cv::Mat& frame)
 {
+	Frame newFrame;
+	newFrame.frame = frame.clone();
+	newFrame.frameIndex = ++frameIdx_;
+
 	std::unique_lock<std::mutex> lock(mutex_);
 	if (buffer_.size() >= maxSize_)
 	{
 		buffer_.pop_front(); // Discard oldest frame
 	}
-	buffer_.push_back(frame.clone());
+	buffer_.push_back(newFrame);
+	cond_.notify_one();
 }
 
 
-bool FrameBuffer::pop(cv::Mat& frame)
+bool FrameBuffer::pop(Frame& outFrame)
 {
 	std::unique_lock<std::mutex> lock(mutex_);
+	cond_.wait(lock, [this] 
+		{ return !buffer_.empty(); });
 	if (buffer_.empty()) 
 		return false;
-    frame = buffer_.back().clone();  // Get latest frame
-    buffer_.clear();                 // Drop old ones
+	outFrame = buffer_.back();		// Get latest frame
+    buffer_.clear();                // Drop old ones
     return true;
+}
+
+Frame FrameBuffer::getFrameByIndex(uint32_t idx)
+{
+	std::unique_lock<std::mutex> lock(mutex_);
+	for (auto& f : buffer_)
+	{
+		if (f.frameIndex == idx)
+		{
+			return f;
+		}
+	}
+	return Frame{}; // Return an empty Frame if not found
 }
